7f9409f304889072dcb876333f10d159c30e4c07
Waled <waled@example.com>
2025-04-22 13:00:50 +0300

    add date in movie card

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -3,7 +3,9 @@ import { useNavigate } from "react-router-dom";
 import React from "react";
 
 const MovieCard = ({ card, onClick }) => {
-  const imageUrl = card.poster_path ? `https://image.tmdb.org/t/p/w500${card.poster_path}` : "https://m.media-amazon.com/images/I/61s8vyZLSzL._AC_UF894,1000_QL80_.jpg";
+  const imageUrl = card.poster_path
+    ? `https://image.tmdb.org/t/p/w500${card.poster_path}`
+    : "https://m.media-amazon.com/images/I/61s8vyZLSzL._AC_UF894,1000_QL80_.jpg";
   return (
     <div className="movie-card" onClick={() => onClick(card)}>
       <img
@@ -11,7 +13,13 @@ const MovieCard = ({ card, onClick }) => {
         alt={card.title}
         className="movie-poster"
       />
-      <h3 className="movie-title">{card.original_title || card.original_name}</h3> {/* Add the movie title */}
+      <h3 className="movie-title">
+        {card.original_title || card.original_name}
+      </h3>{" "}
+      {/* Add the movie title */}
+      <p className="movie-title">
+        {card.release_date?.split("-")[0] || card.first_air_date?.split("-")[0]}
+      </p>
     </div>
   );
 };
